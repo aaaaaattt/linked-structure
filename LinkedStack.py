c011d6d7be9ac817d34c373ce339c2a74e40df4a
ch <chlgur13@kakao.com>
2023-10-17 21:23:54 +0900

    Linked List complete & made Node module

--- LinkedStack.py
+++ LinkedStack.py
@@ -1,7 +1,4 @@
-class Node:
-    def __init__(self,elem,link=None): #링크는 default값으로 None사용
-        self.data = elem #노드 데이터 필드
-        self.link - link #노드 링크 필드
+from Node import Node
 
 class LinkedStack :
     def __init__(self):
